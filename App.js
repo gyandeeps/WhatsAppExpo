4860c85310b958b9b02deedf7887e8700a4d3f8d
Gyandeep Singh <gyandeeps@gmail.com>
2021-01-24 23:29:44 -0600

    New: React native with expo

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator initialRouteName="Home">
+                <Tab.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{ tabBarLabel: "Home" }}
+                />
+                <Tab.Screen
+                    name="Settings"
+                    component={SettingScreen}
+                    options={{ tabBarLabel: "Settings" }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+};
+
+const HomeScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app! HomeScreen</Text>
+            <StatusBar style="light" />
+        </View>
+    );
+};
+
+const SettingScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>
+                Open up App.js to start working on your app! SettingScreen
+            </Text>
+            <StatusBar style="dark" />
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    }
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,1 @@
+# React Native playground with Expo
